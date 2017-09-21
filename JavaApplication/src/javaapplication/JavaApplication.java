73ab8fc62e03418690322efa77c74407d5e49348
kubuntu <kubuntu@kubuntu-VirtualBox>
2017-09-21 07:36:21 +0200

    New Java Application

---
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,16 @@
+package javaapplication;
+
+/**
+ *
+ * @author kubuntu
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
