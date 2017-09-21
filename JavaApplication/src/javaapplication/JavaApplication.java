f1b12f821221ccf51943d24f8ee6da72ca5177a0
kubuntu <kubuntu@kubuntu-VirtualBox>
2017-09-21 08:00:49 +0200

    Insignificant changes to Hello World

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -10,7 +10,6 @@ public class JavaApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-    
+        System.out.println("Hello World!");
+    }    
 }
