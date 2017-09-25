53ececa5ea2805a672b3b4efe94b82c6173c8f30
kubuntu <kubuntu@kubuntu-VirtualBox>
2017-09-25 14:14:20 +0200

    Kennenl. angelegt calender erzeugt

--- JavaApplication/src/Paul/Calender.java
+++ JavaApplication/src/Paul/Calender.java
@@ -10,10 +10,26 @@ package Paul;
  * @author kubuntu
  */
 public class Calender {
+    /**
+     * daysPerMonth ist eine Klassenvariable (static)
+     */
+    public static int[] daysPerMonth = new int[-1, 31, 28, 31, 30, 31, 31, 30, 31, 30, 31];
+    
     private int year;
     private int month;
     private int day;
 
+    /**
+     * isLeapYear ist eine Klassenmethode (static), i.e. "Bibliotheksmethode"
+     * @param year
+     * @return 
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year != 0 || year % 400 == 0);
+    }
+    
+    
+    
     /**
      * 
      * @param year
@@ -29,7 +45,56 @@ public class Calender {
     public static void main(String[] args) {
         Calendar calendar = new Calendar(2017,9,25);
     }
- 
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    
+    /**
+     * Die Methode wechselt zum nächsten Tag
+     */
+    public void nextDay () {
+        int days = Calender.daysPerMonth[this.month];  //expliz. Scheibweise
+        // days = dasePerMonth[month]; // dazu semantisch äquivalent impl. Schreibweise
+        days++;
+        if day > daysPerMonth[month] {
+            day =1;
+            if (isLeapYear(year) {
+                month;
+            }
+            month++;
+            if (month > 12) {
+                month=1;
+                year++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new Calendar(2017, 9 , 30);
+        calendar.nextDay();
+        System.out.println("calender");
+    }
     
     
 }
